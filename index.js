57d9c846b2292b617d45905688a3d88f58fd3ba6
omarfaruk <mdomarfaruq2806@gmail.com>
2026-02-10 14:46:18 +0600

    problem 04

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ function validOtp(otp) {
 
 // console.log(validOtp("ph-helle"));
 
-
 // Problem-03: BCS Final Score Calculator
 
 function finalScore(omr) {
@@ -39,7 +38,31 @@ function finalScore(omr) {
     return "Invalid";
   }
 
-  return Math.round(right - wrong * 0.5)
+  return Math.round(right - wrong * 0.5);
 }
 
 // console.log(finalScore({ right: 50.5, wrong: 10, skip: 39.5 }));
+
+// Problem-04: Upcoming Gono Vote
+
+function gonoVote(votes) {
+  const count1 = votes.filter((vote) => {
+    return vote == "ha";
+  }).length;
+
+  const count2 = votes.filter((vote) => {
+    return vote == "na";
+  }).length;
+
+  if (count1 > count2) {
+    return true;
+  } else if (count2 > count1) {
+    return false;
+  } else {
+    return "equal";
+  }
+}
+
+// console.log(gonoVote(["ha", "na", "ha", "na", "na", "na"]));
+
+
